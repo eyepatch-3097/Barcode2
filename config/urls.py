aca95b73e411b722b1ee4930ee46a08881b2e2fd
Siddharth Das <66112421+eyepatch-3097@users.noreply.github.com>
2025-10-06 23:47:08 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -30,5 +30,9 @@ urlpatterns = [
     path("organizations/", include("organizations.urls")),
 ]
 
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
